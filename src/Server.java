04ae7c05905335b64cd4c15a2d9eef257fa3adb8
kanestapler <kanestapler@gmail.com>
2016-10-17 13:56:26 -0500

    Adding in working server

--- src/Server.java
+++ src/Server.java
@@ -1,9 +1,38 @@
+import java.net.*;
+import java.io.*;
 
 public class Server {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		String message;
+		try {
+			@SuppressWarnings("resource")
+			ServerSocket server = new ServerSocket(4400);
+			while (true){
+				Socket socket = server.accept();
+				BufferedInputStream binst = new
+						BufferedInputStream(socket.getInputStream(),
+								socket.getReceiveBufferSize());
+				System.out.print("\n RECEIVED MESSAGE: ");
+				message = "";
+				for (int i=0;i<socket.getReceiveBufferSize();i++) {
+					int data = binst.read();
+					if (data == -1) break;
+					else{
+						System.out.print ((char) data) ;
+						message += (char) (data);
+					}
+				}
+				message = message.trim();
+				socket.close();
+				if (message.equals("bye")){
+					System.out.println("\n Closing ... goodbye!");
+					break;
+				}
+			}
+		} catch (Exception e) {
+			System.out.println("Error " + e.getMessage());
+		}
 	}
 
 }
